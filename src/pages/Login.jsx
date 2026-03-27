27dc6ae6b102b8967784f0521d8534d76d7dd115
LimPogi <cirolimjoco@gmail.com>
2026-03-28 01:36:27 +0800

    fix casing issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Loader from "../components/Loader";
+import Loader from "../components/layout/Loader";
 
 export default function Login({ setToken, setRole }) {
   const [email, setEmail] = useState("");
