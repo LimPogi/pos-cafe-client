70496e0459f82270ee9e10b6508ab813254ba2ec
LimPogi <cirolimjoco@gmail.com>
2026-03-28 18:25:18 +0800

    update POS system

--- src/pages/Cashier.jsx
+++ src/pages/Cashier.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "../styles/Cashier.css";
 
 export default function Cashier() {
   const API = import.meta.env.VITE_API_URL;
